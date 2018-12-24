53f0a1cd39f04a4e78069c2befe35cbbcf4b0e45
xiao103 <icesugernowater@gmail.com>
2018-12-24 09:12:23 +0800

    v2.9 扩大了 D_Q ，即将 R 设置为 4，最终生成的 D_Q 是四十多万条。预期中的结果是，可以提高 phi_s 的准确率，但事实上并没有。还是 43.2% 的准确率。

--- CONSTS.py
+++ CONSTS.py
@@ -1,13 +1,13 @@
-R = 1
-PATH_D_Q_ROOT = 'D_Q_exp1_v2.7'
+R = 4
+PATH_D_Q_ROOT = 'D_Q_exp1_v2.9'
 
 MODDROP_RATE = 0.5
 
-EXPERIMENT_RESULT_FILE_NAME = 'experiment1_(v2.8)_results.txt'
-D_Q_DATAFRAME_FILE_NAME = 'QoU_to_deltaStar_v2.8.csv'
+EXPERIMENT_RESULT_FILE_NAME = 'experiment1_(v2.9)_results.txt'
+D_Q_DATAFRAME_FILE_NAME = 'QoU_to_deltaStar_v2.9.csv'
 
-LQDataset_on_subset_with_dmgfunc_at_degree_ROOT = 'Expr1_v2.8/'
+LQDataset_on_subset_with_dmgfunc_at_degree_ROOT = 'Expr1_v2.9/'
 
-PREDICTED_DELTAS_PATH = 'predicted_deltas_v2.8/'
+PREDICTED_DELTAS_PATH = 'predicted_deltas_v2.9/'
 
 STEP_TO_NEXT_STBLOCK = 5    # 滑动几帧，继续取下一个 stblock
